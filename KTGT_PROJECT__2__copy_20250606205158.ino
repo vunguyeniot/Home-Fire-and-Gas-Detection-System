37e8e62326e2750c055b362d875e56369ce7886a
vunguyeniot <vunguyeniot@gmail.com>
2025-09-28 11:33:36 +0700

    Home Fire and Gas Detection System

---
+++ KTGT_PROJECT__2__copy_20250606205158.ino
@@ -0,0 +1,304 @@
+#include <LiquidCrystal.h>
+#include <PubSubClient.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <ESP32Servo.h>
+#include <ArduinoJson.h>
+
+#define buttonPinFan    5
+#define buttonPinPump    18
+#define buttonPinDoor    19
+#define buttonPinAutoMode   21
+#define SENSOR_MQ2  35
+#define SENSOR_FIRE   34
+#define SENSOR_FIRE_ON    0
+#define SENSOR_FIRE_OFF   1
+#define BUZZER  23
+#define BUZZER_ON 1
+#define BUZZER_OFF 0
+#define ON 1
+#define OFF 0
+#define RELAY1  22
+#define RELAY2  32
+#define SERVO1 33
+#define SERVO2 25
+#define LCD_RS  15
+#define LCD_EN  13
+#define LCD_D4  12
+#define LCD_D5  14
+#define LCD_D6  27
+#define LCD_D7  26
+LiquidCrystal My_LCD(15, 13, 12, 14, 27, 26);
+const int buttonPins[] = {5, 19, 18, 5};
+const int numButtons = sizeof(buttonPins) / sizeof(buttonPins[0]);  
+
+bool relay1State = OFF;
+bool relay2State = OFF;
+
+int mq2Thresshold = 1000; 
+
+Servo myservo1; 
+Servo myservo2; 
+int window = 0;
+
+const char* ssid = "iPhone";
+const char* pass = "NamTran1998";
+const char* mqtt_server = "172.20.10.9";
+const int mqtt_port = 1883;
+const char* subFan_topic = "Devices/Fan";
+const char* subPump_topic = "Devices/Pump";
+const char* subLCD_topic = "Devices/LCD" ;
+
+TaskHandle_t TaskButton_handle = NULL;
+TaskHandle_t TaskMqtt_handle = NULL;
+
+void esp_sub();
+void pubMQTT(const char * topic, const char* payload);
+void sendDatatoMQTT();
+void TaskMqtt(void *pvParameters);
+void TaskButton(void *pvParameters); 
+void openWindow();
+void closeWindow();
+void controlWindow(int onoff);
+int readFireSensor();
+int readMQ2();
+void LCD1602_Init();
+void LCDPrint(int hang, int cot, char *text, int clearOrNot);
+
+WiFiClient espClient; 
+PubSubClient client(espClient);
+
+void setup() {
+  Serial.begin(115200);
+  LCD1602_Init();
+  
+  //---------- Khai báp WiFi ---------
+  WiFi.begin(ssid, pass); 
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500); 
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi Connected!");
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+  if(client.connect("ESP32_Client")){
+    Serial.println(" Kết nối thành công MQTT");
+    
+  }else{
+    Serial.println("Lỗi kết nối MQTT");
+  }
+
+  pinMode(RELAY1, OUTPUT);                 
+  pinMode(RELAY2, OUTPUT);
+  //pinMode(2,OUTPUT);
+
+
+  pinMode(BUZZER, OUTPUT);  
+  pinMode(SENSOR_FIRE, INPUT_PULLUP);  
+  digitalWrite(BUZZER, BUZZER_OFF);
+
+  for (int i = 0; i < numButtons; i++) {
+        pinMode(buttonPins[i], INPUT_PULLUP);  
+    }
+  
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+  myservo1.setPeriodHertz(50);  
+  myservo2.setPeriodHertz(50);  
+  myservo1.attach(SERVO1, 500, 2400);
+  myservo2.attach(SERVO2, 500, 2400);
+  closeWindow();
+  
+  
+  xTaskCreatePinnedToCore(TaskMqtt,"TaskMqtt" , 1024*2 ,  NULL,  5 ,  &TaskMqtt_handle , 0);
+  xTaskCreatePinnedToCore(TaskButton,"TaskButton" ,1024*4 ,  NULL,  5 ,  &TaskButton_handle , 0);
+
+}
+void loop() {
+}
+
+void esp_sub(){
+    client.subscribe(subFan_topic);
+    client.subscribe(subPump_topic); 
+    client.subscribe(subLCD_topic);
+}
+
+void pubMQTT(const char * topic, const char* payload){
+  if(client.connected()){
+    client.publish(topic, payload);
+  }
+}
+
+
+void sendDatatoMQTT() {
+    StaticJsonDocument<100> gasJson;
+    StaticJsonDocument<100> fireJson;
+
+    gasJson["gas_value"] = readMQ2();
+    fireJson["fire_status"] = readFireSensor();
+
+    // Chuyển JSON thành chuỗi
+    char gasBuffer[100];
+    char fireBuffer[100];
+    serializeJson(gasJson, gasBuffer);
+    serializeJson(fireJson, fireBuffer);
+
+    pubMQTT("esp32/sensor/Gas", gasBuffer);  
+    pubMQTT("esp32/sensor/Flame",fireBuffer);
+    
+    // In ra Serial để kiểm tra
+    Serial.print("Gửi MQTT - Gas: ");
+    Serial.println(gasBuffer);
+
+    Serial.print("Gửi MQTT - Fire: ");
+    Serial.println(fireBuffer);
+}
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+    Serial.print("Nhận dữ liệu từ topic:");
+    Serial.println(topic);
+
+    char message[length + 1];
+    memcpy(message, payload, length);
+    message[length] = '\0'; // Thêm ký tự kết thúc chuỗi
+
+  // kiểm tra message nhận được
+  Serial.println(message);
+  
+   StaticJsonDocument<200> doc; 
+   DeserializationError error = deserializeJson(doc, message);
+   
+
+   if(doc.containsKey("status")) {
+    int status = doc["status"];
+        Serial.print("Trạng thái nhận được: ");
+        Serial.println(status);
+
+        // Xử lý theo từng topic
+        if (strcmp(topic, "Devices/Fan") == 0) {
+            digitalWrite(RELAY1, status ? HIGH : LOW);
+            controlWindow(status);
+            Serial.println(status ? "Quạt BẬT" : "Quạt TẮT");
+        } 
+        else if (strcmp(topic, "Devices/Pump") == 0) {
+            digitalWrite(RELAY2, status ? HIGH : LOW);
+            Serial.println(status ? "Máy bơm BẬT" : "Máy bơm TẮT");
+        }
+   }
+}
+
+void TaskMqtt(void *pvParameters) {
+    while (1) {
+        if (!client.connected()) {  
+            Serial.println("MQTT Disconnected! Reconnecting...");
+            if (client.connect("ESP32_Client")) {
+                Serial.println("MQTT Connected!");
+                esp_sub();
+            }
+        }
+        client.loop();
+        sendDatatoMQTT();  
+        vTaskDelay(pdMS_TO_TICKS(1000));  
+    }
+}
+
+void TaskButton(void *pvParameters) {
+
+    bool lastStateBTN1 = HIGH;
+    bool lastStateBTN2 = HIGH;
+    bool lastStateBTN3 = HIGH;
+    
+    while (1) {
+        bool currentStateBTN1 = digitalRead(5);
+        bool currentStateBTN2 = digitalRead(19);
+        bool currentStateBTN3 = digitalRead(18);
+         
+       
+        if (currentStateBTN1 == LOW && lastStateBTN1 == HIGH) {
+            relay1State = !relay1State;
+            digitalWrite(RELAY1, relay1State ? HIGH : LOW);
+            digitalWrite(2, relay1State ? HIGH : LOW);
+
+            Serial.print("RELAY1: ");
+            Serial.println(relay1State ? "ON" : "OFF");
+        }
+        lastStateBTN1 = currentStateBTN1;
+
+       
+        if (currentStateBTN2 == LOW && lastStateBTN2 == HIGH) {
+            relay2State = !relay2State;
+            digitalWrite(RELAY2, relay2State ? HIGH : LOW);
+            digitalWrite(2, relay1State ? HIGH : LOW);
+            Serial.print("RELAY2: ");
+            Serial.println(relay2State ? "ON" : "OFF");
+        }
+        lastStateBTN2 = currentStateBTN2;
+
+        if (currentStateBTN3 == LOW && lastStateBTN3 == HIGH) {
+            window = !window;
+            controlWindow(window);
+        }
+        lastStateBTN3 = currentStateBTN3;
+
+        vTaskDelay(pdMS_TO_TICKS(50)); 
+    }
+}
+
+
+
+int readFireSensor() {
+    int Fire_Value = !digitalRead(SENSOR_FIRE);
+    if(Fire_Value == 1){
+      digitalWrite(BUZZER,BUZZER_ON);
+    }
+    else {
+      digitalWrite(BUZZER,BUZZER_OFF);
+    }
+    Serial.println(Fire_Value);
+    return Fire_Value;
+}
+
+int readMQ2(){
+
+    float MQ2_Value = analogRead(SENSOR_MQ2);
+    MQ2_Value = map(MQ2_Value, 0 , 4095, 0, 10000 );
+    Serial.println(MQ2_Value);
+    return MQ2_Value;
+}
+
+
+void LCD1602_Init() {
+   My_LCD.begin(16, 2);
+   My_LCD.clear();
+   LCDPrint(0,2,"Gas Detection",0);
+   LCDPrint(1,5,"System",0);
+}
+
+void LCDPrint(int hang, int cot, char *text, int clearOrNot) {
+   if(clearOrNot == 1)
+      My_LCD.clear();
+   My_LCD.setCursor(cot, hang);
+   My_LCD.print(text);
+}
+
+
+void openWindow() {
+    myservo1.write(0); 
+    myservo2.write(180);
+}
+
+void closeWindow() {
+    myservo1.write(90); 
+    myservo2.write(90);
+
+}
+void controlWindow(int onoff) {
+    if(onoff == 0)
+      closeWindow();
+    else
+      openWindow();
+
+}
